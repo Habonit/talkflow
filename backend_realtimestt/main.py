04eda990c3b984874871d08fa513ca3fac4bd587
paradeigma <khk172216@gmail.com>
2025-06-15 00:11:20 +0900

    [refactor] 디렉토리 구조 변경 및 경로 수정
    메인 애플리케이션 파일을 백엔드 디렉토리로 이동하고 관련된 엔트리 포인트를 수정했습니다. 코드의 관리 효율성을 높이기 위한 구조 개선 작업입니다.

--- backend_realtimestt/main.py
+++ backend_realtimestt/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers.realtimestt import router as stt_router
+from backend_realtimestt.routers.realtimestt import router as stt_router
 
 app = FastAPI()
 app.include_router(stt_router)
